1e29f8ce6cff889843b968718b365cf4763d5cdc
halfbro <mlagaren@stevens.edu>
2017-05-09 20:32:44 -0400

    Add SDL to project and create test window

---
+++ mips_cpe-462/mips_cpe-462/main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <iostream>
+//Using SDL and standard IO
+#include <SDL2-2.0.5\include\SDL.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main ( int argc, char *argv[] ) {
+
+	 //The window we'll be rendering to
+    SDL_Window* window = NULL;
+    
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    } else {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL ) {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        } else {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+        }
+	}
+
+	printf("Hello World");
+	getchar();
+
+	//Destroy window
+    SDL_DestroyWindow( window );
+
+    //Quit SDL subsystems
+    SDL_Quit();
+	
+	return 0;
+}
