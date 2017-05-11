b1259095a34db316d0b07f8fd81b91f9c92c4691
halfbro <mlagaren@stevens.edu>
2017-05-10 23:56:21 -0400

    Add loading of test file

--- mips_cpe-462/mips_cpe-462/main.cpp
+++ mips_cpe-462/mips_cpe-462/main.cpp
@@ -7,7 +7,81 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+class Model {
+private:
+	int *data;
+	int w, l, h;
+
+public:
+	Model(int width, int length, int height);
+	~Model();
+	
+	int getIntensityAt(int x, int y, int z);
+	void readFromFile(FILE *fd);
+};
+
+Model::Model(int width, int length, int height) {
+	w = width;
+	l = length;
+	h = height;
+	data = new int[w*l*h];
+}
+
+Model::~Model() {
+	delete[] data;
+}
+
+int Model::getIntensityAt(int x, int y, int z) {
+	if (x > w || x < 0) return 0;
+	if (y > l || y < 0) return 0;
+	if (z > h || z < 0) return 0;
+
+	return data[x*l*h + y*h + z];
+}
+
+void Model::readFromFile(FILE *fd) {
+	for (int i=0; i<h; ++i) {
+		for (int j=0; j<l; ++j) {
+			for (int k=0; k<w; ++k) {
+				fscanf(fd, "%i", &data[i*w*l + j*w + k]);
+			}
+		}
+	}
+}
+
 int main ( int argc, char *argv[] ) {
+
+	if (argc != 2) {
+		printf("Wrong number of arguments used, argument should be filename\n");
+		getchar();
+		return 0;
+	}
+
+	printf("Loading file %s\n", argv[1]);
+
+	bool modelvalid = true;
+
+	FILE *fd = fopen(argv[1], "r");
+	if (fd == NULL) {
+		printf("Error opening file\n");
+		getchar();
+		return 0;
+	}
+
+	int length, width, height;
+	char fileheader[4];
+	fscanf(fd, "%s %i %i %i", fileheader, &width, &length, &height);
+	printf("w:%i l:%i h:%i\n", width, length, height);
+	getchar();
+
+	if ( !modelvalid ) {
+		printf("File could not be read\n");
+		getchar();
+		return 0;
+	}
+
+	Model model(width, length, height);
+	model.readFromFile(fd);
 
 	 //The window we'll be rendering to
     SDL_Window* window = NULL;
@@ -35,8 +109,41 @@ int main ( int argc, char *argv[] ) {
         }
 	}
 
-	printf("Hello World");
-	getchar();
+	bool quit = false;
+	SDL_Event e;
+
+	int orientation[3] = {0, 10, 0};
+
+	while (!quit) {
+		//Handle events on queue
+        while( SDL_PollEvent( &e ) != 0 ) {
+			switch (e.type) {
+			case SDL_QUIT:
+				quit = true;
+				break;
+			case SDL_MOUSEMOTION:
+				// If left mouse button is held
+				if (e.motion.state & SDL_BUTTON_LMASK) {
+					orientation[0] += e.motion.xrel;
+					orientation[2] -= e.motion.yrel;
+				}
+				printf("X: %i, Y: %i, Z: %i\n", orientation[0], orientation[1], orientation[2]);
+				break;
+			case SDL_MOUSEWHEEL:
+				orientation[1] -= e.wheel.y;
+				printf("X: %i, Y: %i, Z: %i\n", orientation[0], orientation[1], orientation[2]);
+				break;
+			}
+        }
+
+		SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0x00 ) );
+
+		// TODO: get the image from Gary's projection
+		//SDL_Surface* imageout = SDL_CreateRGBSurface( 0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0, 0, 0, 0 );
+		
+		//Update the surface
+        SDL_UpdateWindowSurface( window );
+	}
 
 	//Destroy window
     SDL_DestroyWindow( window );
